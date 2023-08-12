4ae07b71f4d3969a629427a31d58907af9b2e9d0
Lrae <109258251+Lrae1207@users.noreply.github.com>
2023-08-12 16:28:33 -0700

    Major changes to terminal app

--- src/sniffer.cpp
+++ src/sniffer.cpp
@@ -1,20 +1,64 @@
 //http://yuba.stanford.edu/~casado/pcap/section1.html
-// I now realize that I may need a variety of different versions of this file as there are many
-// different operating systems that use different methods of socket programming
-#include <pcap.h>
+//https://dev.to/fmtweisszwerg/cc-how-to-get-all-interface-addresses-on-the-local-device-3pki
+#include <iostream>
+#include "sniffer.hpp"
 #include <fstream>
 #include <sys/socket.h>
 #include <sys/types.h>
+
+std::vector<interface> getInterfaces() {
+    std::vector<interface> interfaces;
+    struct ifaddrs* p_iffirst;
+    
+    if (getifaddrs(&p_iffirst) == 0) {
+        for (struct ifaddrs* p_ifaddr = p_iffirst; p_ifaddr != nullptr; p_ifaddr = p_ifaddr->ifa_next) {
+            interface interf;
+
+            interf.name = p_ifaddr->ifa_name;
+            
+            sa_family_t addr_family = p_ifaddr->ifa_addr->sa_family;
+            interf.sa_family = addr_family;
+            // IPv4
+            if (addr_family == AF_INET) {
+                if (p_ifaddr->ifa_addr != nullptr) {
+                    inet_ntop(addr_family,&((struct sockaddr_in*)(p_ifaddr->ifa_addr))->sin_addr, interf.ip_4, INET_ADDRSTRLEN);
+                }
+                if (p_ifaddr->ifa_netmask != nullptr) {
+                    inet_ntop(addr_family,&((struct sockaddr_in*)(p_ifaddr->ifa_netmask))->sin_addr, interf.nmsk_4, INET_ADDRSTRLEN);
+                }
+            } else if (addr_family == AF_INET6) {
+
+            }
+
+            interfaces.push_back(interf);
+        }
+    }
+}
 
 int writeToFile() {
     
 }
 
 int main(int argc, char *argv[]) {
-    // Command format is: argv[1] <filename> <media identifier(im not sure what yet)> 
-    if (argc != 3) {
+    // Parameter handling
+    if (argc != 1) {
+        std::cout << "Invalid amount of parameters. \nusage: " << argv[0] << "\n";
         exit(1);
     }
+
+    std::cout << "Searching for interfaces...\n";
+    std::vector<interface> availInterfaces;
+    availInterfaces = getInterfaces();
+
+    for (int i = 0; i < availInterfaces.size(); ++i) {
+        interface interf = availInterfaces[i];
+        if (interf.addr_family == AF_INET) { // IPv4 address
+            std::cout << "\tInterface \"" << interf.name << "\",\t" << "Ip: " << interf.ip_4 << ",\tNetmask: " >> interf.nmsk_4 << ".\n";
+        } else {    // Must be IPv6
+            std::cout << "\tInterface \"" << interf.name << "\"," << "Ip: " << interf.ip_4 << "\n";
+        }
+    }
+
 
     // Main file that will be written to
     std::ofstream datafile(argv[1]);
@@ -23,5 +67,5 @@ int main(int argc, char *argv[]) {
 
     
     
-    return 0;
+    exit(0);
 }
---
+++ src/sniffer.hpp
@@ -0,0 +1,16 @@
+#include <pcap.h>
+#include <string>
+#include <vector>
+
+struct interface {
+    sa_family_t sa_family;
+    std::string name;
+
+    char ip_4[INET_ADDRSTRLEN];
+    char nmsk_4[INET_ADDRSTRLEN];
+
+    char ip_6[INET6_ADDRSTRLEN];
+    char nmsk_6[INET6_ADDRSTRLEN];
+};
+
+std::vector<interface> getInterfaces();
