13005e1e669199ba28892ad3a5923d207931e8ad
Lrae <109258251+Lrae1207@users.noreply.github.com>
2023-08-15 21:27:17 -0700

    Update sniffer.hpp

--- src/sniffer.hpp
+++ src/sniffer.hpp
@@ -5,6 +5,24 @@
 #include <net/ethernet.h>
 #include <netinet/ether.h>
 
+// stolen from pcap site
+struct my_ip {
+	u_int8_t	ip_vhl;		/* header length, version */
+#define IP_V(ip)	(((ip)->ip_vhl & 0xf0) >> 4)
+#define IP_HL(ip)	((ip)->ip_vhl & 0x0f)
+	u_int8_t	ip_tos;		/* type of service */
+	u_int16_t	ip_len;		/* total length */
+	u_int16_t	ip_id;		/* identification */
+	u_int16_t	ip_off;		/* fragment offset field */
+#define	IP_DF 0x4000			/* dont fragment flag */
+#define	IP_MF 0x2000			/* more fragments flag */
+#define	IP_OFFMASK 0x1fff		/* mask for fragmenting bits */
+	u_int8_t	ip_ttl;		/* time to live */
+	u_int8_t	ip_p;		/* protocol */
+	u_int16_t	ip_sum;		/* checksum */
+	struct	in_addr ip_src,ip_dst;	/* source and dest address */
+};
+
 struct interface {
     sa_family_t sa_family;
     std::string name;
@@ -23,4 +41,17 @@ struct ether_data {
     u_int16_t type;
 };
 
+struct ip_data {
+    int hdrlen;
+    std::string src;
+    std::string dest;
+    int version;
+    int len;
+    int offset;
+};
+
+struct packet_data {
+    int index;
+    ether_data eth;
+};
 void printClamped(std::string buffer, int n, std::string color);
