bf55289c250785daa67a9315b8fc65fd1cb46d02
Lrae <lraeprogramming@gmail.com>
2024-01-02 22:34:48 -0800

    Update compile.sh

--- source/compile.sh
+++ source/compile.sh
@@ -1,3 +1,4 @@
 # Compiles and executes the program
-g++ sniffer.cpp sniffer.hpp colors.hpp -lpcap
-sudo ./a.out
+g++ sniffer.cpp sniffer.hpp colors.hpp -lpcap -o packet_sniffer
+sudo ./packet_sniffer
+rm packet_sniffer
