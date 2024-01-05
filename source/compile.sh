992a8b410146458570747dd4664ee250143f8da3
Lrae <lraeprogramming@gmail.com>
2024-01-05 13:58:31 -0800

    Update compile.sh

--- source/compile.sh
+++ source/compile.sh
@@ -1,4 +1,42 @@
 # Compiles and executes the program
-g++ sniffer.cpp sniffer.hpp colors.hpp -lpcap -o packet_sniffer
-sudo ./packet_sniffer
-rm packet_sniffer
+if [ ! -f "./sniffer.cpp" ]
+then
+    echo "sniffer.cpp not found"
+    exit 1
+fi
+
+if [ ! -f "./sniffer.hpp" ]
+then
+    echo "sniffer.hpp not found"
+    exit 1
+fi
+
+if [ ! -f "./colors.hpp" ]
+then
+    echo "colors.hpp not found"
+    exit 1
+fi
+
+if [ ! -f "./tcp_seq.h" ]
+then
+    echo "tcp_seq.h not found"
+    exit 1
+fi
+
+echo Compiling...
+
+g++ sniffer.cpp sniffer.hpp colors.hpp -lpcap -o "execfile"
+
+if [ $? -eq 0 ]:
+then
+    echo "Compilation successful"
+    echo -n "Execute it? y/n? "
+    input=""
+    read input
+    if [ $input = "y" ]
+    then
+        sudo ./"execfile"
+    fi
+else
+    echo "Compilation failed"
+fi
